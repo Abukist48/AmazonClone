31c90f0912a105cefa3b981d2a46fc4fd60ab4da
Abukist48 <abukist48@gmail.com>
2023-07-10 13:42:41 +0300

    ten

--- amazon-clone/src/Components/axios.js
+++ amazon-clone/src/Components/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-	// baseURL: "http://localhost:3000",
-	baseURL: "https://zealous-twill-pig.cyclic.app/",
+	baseURL: "http://localhost:10000",
+	// baseURL: "https://zealous-twill-pig.cyclic.app/",
 });
 
 export default instance;
